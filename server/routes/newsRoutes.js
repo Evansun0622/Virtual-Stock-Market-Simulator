4747938595ad1c6337084c6c197698d11f107196
evansun0622 <evanyy@LAPTOP-TN8QPCTU.localdomain>
2020-11-17 19:32:10 -0400

    server error

--- server/routes/newsRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getNewsData } = require("../controllers/newsController");
-
-router.route("/").get(getNewsData);
-
-module.exports = router;
